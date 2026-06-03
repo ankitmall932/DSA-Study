25162b2bba6e4b1e684f715a3bb0518f6ec37766
Ankit Mall <ankitmall932@gmail.com>
2026-06-03 13:58:56 +0530

    03 input detail

--- 01 Variables and Data Types/JavaBasic.java
+++ 01 Variables and Data Types/JavaBasic.java
@@ -1,11 +1,45 @@
-//boiler plate code - this two line of code is necessary in all java code
+import java.util.*;
+
 public class JavaBasic{
     public static void main(String args[]){
-        int a = 5;
-        int b=10;
-        int c=a+b;
-        System.out.println(c);
-        System.out.println("hello ankit");
-        System.out.println("* * * * \n* * * \n* * \n*");
+/*     System.out.println("Enter Your First Name");
+    Scanner scn=new Scanner(System.in);
+    String name=scn.next();
+    System.out.println("Enter your last name");
+    String lname=scn.next();
+    System.out.println("Enter Your Full Address");
+    String add=scn.nextLine();
+    System.out.println(name);
+    System.out.println(lname);
+    System.out.println(add); */
+    //Sum of two numbers
+/*     System.out.println("Calculate the Sum of Two numbers given by users");
+    System.out.println("Please Enter Your first number");
+    Scanner scn=new Scanner(System.in);
+    int a=scn.nextInt();
+    System.out.println("Enter your second Number");
+    int b=scn.nextInt();
+    System.out.println("Sum of your no is:");
+    int sum=a + b;
+    System.out.println(sum); */
+    //product of two numbers
+/*     System.out.println("Calculate the Product of Two numbers given by users");
+    System.out.println("Please Enter Your first number");
+    Scanner scn=new Scanner(System.in);
+    int a=scn.nextInt();
+    System.out.println("Enter your second Number");
+    int b=scn.nextInt();
+    System.out.println("Product of your no is:");
+    int product=a * b;
+    System.out.println(product);  */
+    //Area of circle
+    System.out.println("Calculate the Area of the circle");
+    System.out.println("Please Enter Your circle Radius");
+    Scanner scn=new Scanner(System.in);
+    float a=scn.nextFloat();
+    System.out.println("Radius of circle is :");
+    float r=3.14f;
+    float area=r*a*a;
+    System.out.println(area); 
     }
 } 
