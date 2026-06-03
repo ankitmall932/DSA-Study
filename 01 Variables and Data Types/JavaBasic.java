fc5be136dbc75bce299b461b837ea178737903e7
Ankit Mall <ankitmall932@gmail.com>
2026-06-03 12:44:54 +0530

    02 basic of java

--- 01 Variables and Data Types/JavaBasic.java
+++ 01 Variables and Data Types/JavaBasic.java
@@ -1,6 +1,11 @@
 //boiler plate code - this two line of code is necessary in all java code
 public class JavaBasic{
     public static void main(String args[]){
-
+        int a = 5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+        System.out.println("hello ankit");
+        System.out.println("* * * * \n* * * \n* * \n*");
     }
-}
+} 
