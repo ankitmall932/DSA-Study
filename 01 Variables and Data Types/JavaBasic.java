648fa34c2dc39a3c23fee041c3e869ba3899a6d9
Ankit Mall <ankitmall932@gmail.com>
2026-06-07 10:40:49 +0530

    Pattern 1 (nested loog)

--- 01 Variables and Data Types/JavaBasic.java
+++ 01 Variables and Data Types/JavaBasic.java
@@ -2,107 +2,41 @@ import java.util.*;
 
 public class JavaBasic{
     public static void main(String args[]){
-        //for loop
-    /*     for(int i=0; i<=100; i++){
-            System.out.println("hello ankit");
-        } */
-       //while loop
-/*         int counter = 0;
-        while (counter<=100) {
-        System.out.println(counter);
-        counter++;
-        } */
-        /* System.out.println("Please enter the range");
-        Scanner scn=new Scanner(System.in);
-        int range=scn.nextInt();
-        int counter=1;
-        while (counter<=range) {
-            System.out.println(counter);
-            counter++;
-        } */
-/*         System.out.println("Please enter the range");
-        Scanner scn=new Scanner(System.in);
-        int range=scn.nextInt();
-        int counter=1;
-        int sum=0;
-        while (counter<=range) {
-            System.out.println(counter);
-            sum=sum+counter;
-            counter++;
-        }
-        System.out.println("The sum = " + sum); */
-
-        //stars print
-/*         for(int i=1;i<=4;i++){
-            System.out.println("****");
-        } */
-
-        //reverse number
-/*         System.out.println("Please Enter the number");
-        Scanner scn=new Scanner(System.in);
-        int number=scn.nextInt();
-        while(number>0){
-            int lastDigit=number%10; // Kisi bhi number ka last digit nikalna ho to modulo ka use hota hai
-            System.out.print(lastDigit);
-            number=number/10; //Kisi bhi number ke last digit ko hatana ho to divide ka use hota hai.
-        } */
-
-        //reverse the number and save in a new variable
-   /*      System.out.println("Please Enter the number");
-        Scanner scn=new Scanner(System.in);
-        int number=scn.nextInt();
-        int reverse=0;
-        while(number>0){
-            int lastDigit=number%10;
-            reverse=(reverse*10)+lastDigit;
-            number=number/10;                     
-        }
-        System.out.print(reverse);*/
-
-        //break
-/*         for(int i=1;i<=10;i++){
-            if(i==5){
-                break;
+        //Nested Loops
+/*         for(int i=1;i<=5;i++){
+            for(int j=1 ;j<=i;j++){
+                System.out.print("*");
             }
-            System.out.println(i);
-        }
-        System.out.println("I am out of the loop"); */
-/*         Scanner scn=new Scanner(System.in);
-        while (true) {
-                    System.out.print("Please Enter the number : ");
-            int n=scn.nextInt();
-            if (n%10==0) {
-                break;
-            }
-            System.out.println(n);
-        }
-        System.out.println("You enter a number which is totally divisible by 10.");*/
+            System.out.println();
+        } */
 
-        //continue
-/*                 Scanner scn=new Scanner(System.in);
-        while (true) {
-                    System.out.print("Please Enter the number : ");
-            int n=scn.nextInt();
-            if (n%10==0) {
-                continue;
+            //reverse
+/*             int n=5;
+            for(int i=1;i<=n;i++){
+            for(int j=1 ;j<=(n-i+1);j++){
+                System.out.print("*");
             }
-            System.out.println(n);
-        } //it is use to skip a particular value or a particular range. */
+            System.out.println();
+        } */
 
-        //number is prime or not
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Please enter the number to check ");
-        int n=sc.nextInt();
-        boolean isPrime=true;
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-                isPrime=false;
+            //half 
+            /* int n=79;
+            for(int i=1;i<=n;i++){
+            for(int j=1 ;j<=i;j++){
+                System.out.print(j);
             }
-        }
-        if(isPrime==true){
-            System.out.println(n + " is prime");
-        }else{
-            System.out.println(n + " is not prime");
-        }
+            System.out.println();
+        } */
+
+            //character
+            int n=6;
+            char ch='A';
+            for(int i=1;i<=n;i++){
+            for(int j=1 ;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        } //ch automatically fetch all the characters.
 } 
 }  
